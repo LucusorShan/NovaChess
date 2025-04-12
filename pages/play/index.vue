4fe9a1d8b4f7b770af2368ab9edb71ee575caf6a
Schuxiong <741400166@qq.com>
2025-04-12 21:49:59 +0800

    初始化提交：NovaChess小程序代码

--- pages/play/index.vue
+++ pages/play/index.vue
@@ -67,19 +67,148 @@
         </view>
       </view>
     </view>
+    
+    <!-- 添加历史对局弹窗 -->
+    <uni-popup ref="historyPopup" type="center">
+      <view class="popup-container history-popup">
+        <view class="popup-header">
+          <text class="popup-title">历史对局</text>
+          <view class="close-btn" @click="closePopup('history')">
+            <image src="https://pic1.imgdb.cn/item/67f3c64fe381c3632bee9038.png" mode="aspectFit"></image>
+          </view>
+        </view>
+        
+        <view class="games-list">
+          <view 
+            v-for="(game, index) in historyGames" 
+            :key="index" 
+            class="game-item"
+            @click="gotoReplay(game.id)"
+          >
+            <view class="time-icon">
+              <image class="clock-icon" src="https://pic1.imgdb.cn/item/67f3c5ffe381c3632bee9011.png" mode="aspectFit"></image>
+            </view>
+            <view class="game-info">
+              <view class="players-info">
+                <text class="player-name">{{game.player1}} ({{game.rating1}})</text>
+                <text class="vs-text">{{game.score}}</text>
+                <text class="player-name">{{game.player2}} ({{game.rating2}})</text>
+              </view>
+            </view>
+            <view class="game-duration">
+              <text class="duration-text">{{game.duration}}</text>
+            </view>
+          </view>
+        </view>
+      </view>
+    </uni-popup>
+    
+    <!-- 添加排行榜弹窗 -->
+    <uni-popup ref="rankPopup" type="center">
+      <view class="popup-container rank-popup">
+        <view class="popup-header">
+          <text class="popup-title">排行榜</text>
+          <view class="close-btn" @click="closePopup('rank')">
+            <image src="https://pic1.imgdb.cn/item/67f3c64fe381c3632bee9038.png" mode="aspectFit"></image>
+          </view>
+        </view>
+        
+        <view class="rank-list">
+          <view 
+            v-for="player in rankingList" 
+            :key="player.rank" 
+            class="rank-item"
+          >
+            <text class="rank-number" :class="{'top1': player.rank === 1, 'top2': player.rank === 2, 'top3': player.rank === 3}">{{player.rank}}</text>
+            <image class="player-avatar" :src="player.avatar" mode="aspectFill"></image>
+            <view class="player-info">
+              <text class="player-name">{{player.name}}</text>
+              <view class="player-country">
+                <image class="country-flag" :src="`/static/images/flags/${player.country}.png`" mode="aspectFit"></image>
+                <text class="country-name">{{player.country.toUpperCase()}}</text>
+              </view>
+            </view>
+            <text class="player-rating">{{player.rating}}</text>
+          </view>
+        </view>
+        
+        <view class="user-rank-section">
+          <view class="rank-item user-rank">
+            <text class="rank-number">{{userRanking.rank}}</text>
+            <image class="player-avatar" :src="userRanking.avatar" mode="aspectFill"></image>
+            <view class="player-info">
+              <text class="player-name">{{userRanking.name}}</text>
+              <view class="player-country">
+                <image class="country-flag" :src="`/static/images/flags/${userRanking.country}.png`" mode="aspectFit"></image>
+                <text class="country-name">{{userRanking.country.toUpperCase()}}</text>
+              </view>
+            </view>
+            <text class="player-rating">{{userRanking.rating}}</text>
+          </view>
+        </view>
+      </view>
+    </uni-popup>
   </view>
 </template>
 
 <script>
 import TopSpacing from '@/components/TopSpacing.vue'
+import UniPopup from '@/components/uni-popup/uni-popup.vue'
 
 export default {
   components: {
-    TopSpacing
+    TopSpacing,
+    UniPopup
   },
   data() {
     return {
-      statusBarHeight: 0
+      statusBarHeight: 0,
+      // 添加历史对局数据
+      historyGames: [
+        { 
+          id: '1001', 
+          player1: 'lucasshanchuxiong', 
+          rating1: '221', 
+          player2: 'dawas39', 
+          rating2: '410', 
+          score: '0-1', 
+          duration: '10 min' 
+        },
+        { 
+          id: '1002', 
+          player1: 'Wang_JB', 
+          rating1: '1530', 
+          player2: 'GrandMaster2023', 
+          rating2: '1498', 
+          score: '1-0', 
+          duration: '15 min' 
+        },
+        { 
+          id: '1003', 
+          player1: 'ChessMaster88', 
+          rating1: '1842', 
+          player2: 'theloyalwolf', 
+          rating2: '1820', 
+          score: '½-½', 
+          duration: '5 min' 
+        }
+      ],
+      // 排行榜数据
+      rankingList: [
+        { rank: 1, name: 'Bigfish1995', rating: 2807, country: 'cn', avatar: 'https://pic1.imgdb.cn/item/67f3c5c7e381c3632bee8ff9.png' },
+        { rank: 2, name: 'CogieMekutune', rating: 2799, country: 'jp', avatar: 'https://pic1.imgdb.cn/item/67f3c5c7e381c3632bee8ff9.png' },
+        { rank: 3, name: 's4gVoid', rating: 2720, country: 'de', avatar: 'https://pic1.imgdb.cn/item/67f3c5c7e381c3632bee8ff9.png' },
+        { rank: 4, name: 'theloyalwolf', rating: 2654, country: 'us', avatar: 'https://pic1.imgdb.cn/item/67f3c5c7e381c3632bee8ff9.png' },
+        { rank: 5, name: 'NikoTheodo', rating: 2630, country: 'pl', avatar: 'https://pic1.imgdb.cn/item/67f3c5c7e381c3632bee8ff9.png' }
+      ],
+      // 用户排名
+      userRanking: {
+        rank: 328,
+        name: '我的用户名',
+        rating: 335,
+        country: 'cn',
+        avatar: 'https://pic1.imgdb.cn/item/67f3c5c7e381c3632bee8ff9.png'
+      }
     }
   },
   onLoad() {
@@ -119,17 +248,29 @@ export default {
     handleToolClick(type) {
       switch (type) {
         case 'history':
-          uni.showToast({
-            title: '查看游戏历史',
-            icon: 'none'
-          });
+          // 打开历史弹窗
+          this.$refs.historyPopup.open();
           break;
         case 'rank':
-          uni.showToast({
-            title: '查看排行榜',
-            icon: 'none'
-          });
+          // 打开排行榜弹窗
+          this.$refs.rankPopup.open();
           break;
+      }
+    },
+    // 跳转到回放页面
+    gotoReplay(gameId) {
+      uni.navigateTo({
+        url: `/pages/play/replay/index?id=${gameId}`
+      });
+      // 关闭弹窗
+      this.$refs.historyPopup.close();
+    },
+    // 关闭弹窗
+    closePopup(type) {
+      if (type === 'history') {
+        this.$refs.historyPopup.close();
+      } else if (type === 'rank') {
+        this.$refs.rankPopup.close();
       }
     }
   }
@@ -236,6 +377,177 @@ export default {
         font-size: 32rpx;
       }
     }
+  }
+}
+
+// 弹窗样式
+.popup-container {
+  width: 680rpx;
+  background-color: rgba(30, 20, 10, 0.9);
+  border-radius: 20rpx;
+  padding: 30rpx;
+  
+  .popup-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20rpx;
+    
+    .popup-title {
+      color: white;
+      font-size: 36rpx;
+      font-weight: bold;
+    }
+    
+    .close-btn {
+      width: 60rpx;
+      height: 60rpx;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      
+      image {
+        width: 40rpx;
+        height: 40rpx;
+      }
+    }
+  }
+}
+
+// 历史对局弹窗样式
+.history-popup {
+  height: 800rpx;
+  
+  .games-list {
+    max-height: 700rpx;
+    overflow-y: scroll;
+    
+    .game-item {
+      display: flex;
+      align-items: center;
+      padding: 15rpx 0;
+      border-bottom: 1rpx solid rgba(255,255,255,0.1);
+      
+      .time-icon {
+        margin-right: 20rpx;
+        
+        .clock-icon {
+          width: 40rpx;
+          height: 40rpx;
+        }
+      }
+      
+      .game-info {
+        flex: 1;
+        
+        .players-info {
+          display: flex;
+          align-items: center;
+          
+          .player-name {
+            color: white;
+            font-size: 28rpx;
+            max-width: 240rpx;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+          }
+          
+          .vs-text {
+            color: #e1b964;
+            margin: 0 10rpx;
+            font-size: 28rpx;
+          }
+        }
+      }
+      
+      .game-duration {
+        .duration-text {
+          color: #999;
+          font-size: 24rpx;
+        }
+      }
+    }
+  }
+}
+
+// 排行榜弹窗样式
+.rank-popup {
+  height: 800rpx;
+  
+  .rank-list {
+    max-height: 600rpx;
+    overflow-y: scroll;
+    
+    .rank-item {
+      display: flex;
+      align-items: center;
+      padding: 20rpx 0;
+      border-bottom: 1rpx solid rgba(255,255,255,0.1);
+      
+      &.user-rank {
+        background-color: rgba(255,255,255,0.1);
+        border-radius: 10rpx;
+      }
+      
+      .rank-number {
+        width: 80rpx;
+        text-align: center;
+        color: white;
+        font-size: 32rpx;
+        font-weight: bold;
+        
+        &.top1 { color: gold; }
+        &.top2 { color: silver; }
+        &.top3 { color: #cd7f32; }
+      }
+      
+      .player-avatar {
+        width: 80rpx;
+        height: 80rpx;
+        border-radius: 40rpx;
+        margin: 0 20rpx;
+      }
+      
+      .player-info {
+        flex: 1;
+        
+        .player-name {
+          color: white;
+          font-size: 32rpx;
+          font-weight: bold;
+          margin-bottom: 10rpx;
+        }
+        
+        .player-country {
+          display: flex;
+          align-items: center;
+          
+          .country-flag {
+            width: 30rpx;
+            height: 20rpx;
+            margin-right: 10rpx;
+          }
+          
+          .country-name {
+            color: #999;
+            font-size: 24rpx;
+          }
+        }
+      }
+      
+      .player-rating {
+        color: #e1b964;
+        font-size: 36rpx;
+        font-weight: bold;
+      }
+    }
+  }
+  
+  .user-rank-section {
+    margin-top: 20rpx;
+    padding-top: 20rpx;
+    border-top: 1rpx solid rgba(255,255,255,0.2);
   }
 }
 </style> 
